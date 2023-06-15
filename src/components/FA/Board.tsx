549b2caa695551d230eafa6241c5d55c74a677ba
Johndel <johndelencabo@gmail.com>
2023-06-15 16:07:20 +0800

    Added the problem 2

--- src/components/FA/Board.tsx
+++ src/components/FA/Board.tsx
@@ -1,23 +1,15 @@
 "use client"
-import State from './State'
-import Xarrow, {Xwrapper} from 'react-xarrows'
+
+import  {Xwrapper} from 'react-xarrows'
 import {useProblem} from '@/context/problem'
 import { useTheme } from '@/context/theme'
+import DFA1 from './DFA1'
+import DFA2 from './DFA2'
 
 
 const Board = () => {
     const {state} = useProblem();
     const {theme} = useTheme();
-    // const arrows = Object.keys(state.dfa.delta).map((key, index) => {
-    //     const [from, val] = key.split('.')
-    //     const to = state.dfa.delta[key]
-    //     // console.log("From: ", from, ", To: ", to, "value: ", val)
-    //     return (
-            
-    //         <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels={val} path='straight' animateDrawing color={theme=== 'Dark'? "#3498DB":"#999"} key={index} start={from} end={to} />
-            
-    //     )
-    // })
     
     return (
         <>
@@ -37,75 +29,10 @@ const Board = () => {
                 {/* {
                     arrows
                 } */}
-                {/* Q0 */}
-                <div className={`absolute flex justify-center items-center top-[40%] left-[5rem]`}>
-                    <State id={'q0'} state={'q0'} finalState={state.dfa.F.includes('q0')} valid={state.dfa.path[state.simulation.step] === 'q0' ? true:false}/>
-                </div>
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="a" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q0.a'} start={'q0'} end={'q1'} />
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="b" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q0.b'} start={'q0'} end={'q2'} />
-                {/* Q1 */}
-                <div className={`absolute flex justify-center items-center top-[20%] left-[20rem]`}>
-                    <State id={'q1'} state={'q1'} finalState={state.dfa.F.includes('q1')} valid={state.dfa.path[state.simulation.step] === 'q1' ? true:false}/>
-                </div>
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="a" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q1.a'} start={'q1'} end={'q3'} />
-                {/* Q2 */}
-                <div className={`absolute flex justify-center items-center top-[60%] left-[20rem]`}>
-                    <State id={'q2'} state={'q2'} finalState={state.dfa.F.includes('q2')} valid={state.dfa.path[state.simulation.step] === 'q2' ? true:false}/>
-                </div>
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="b" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q2.b'} start={'q2'} end={'q4'} />
-                {/* Q3 */}
-                <div className={`absolute flex justify-center items-center top-[20%] left-[40rem]`}>
-                    <State id={'q3'} state={'q3'} finalState={state.dfa.F.includes('q3')} valid={state.dfa.path[state.simulation.step] === 'q3' ? true:false}/>
-                </div>
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="a" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q3.a'} start={'q3'} end={'q5'} />
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="b" path='straight' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q3.b'} start={'q3'} end={'q6'} />
-                {/* Q4 */}
-                <div className={`absolute flex justify-center items-center top-[60%] left-[40rem]`}>
-                    <State id={'q4'} state={'q4'} finalState={state.dfa.F.includes('q4')} valid={state.dfa.path[state.simulation.step] === 'q4' ? true:false}/>
-                </div>
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="b" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q4.b'} start={'q4'} end={'q6'} />
-                <Xarrow endAnchor={'left'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="a" path='straight' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q4.a'} start={'q4'} end={'q5'} />
-                {/* Q5 */}
-                <div className={`absolute flex justify-center items-center top-[20%] left-[60rem]`}>
-                    <State id={'q5'} state={'q5'} finalState={state.dfa.F.includes('q5')} valid={state.dfa.path[state.simulation.step] === 'q5' ? true:false}/>
-                </div>
-                <Xarrow _cpx2Offset={-30} _cpx1Offset={-30}  endAnchor={'left'} startAnchor={'left'}  strokeWidth={3} zIndex={-100} labels="b" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q5.b'} start={'q5'} end={'q6'} />
-                <Xarrow _cpy1Offset={-100} _cpx1Offset={200} _cpx2Offset={100} endAnchor={'right'} startAnchor={'top'}  strokeWidth={3} zIndex={-100} labels="a" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q5.a'} start={'q5'} end={'q5'} />
-                {/* Q6 */}
-                <div className={`absolute flex justify-center items-center top-[60%] left-[60rem]`}>
-                    <State id={'q6'} state={'q6'} finalState={state.dfa.F.includes('q6')} valid={state.dfa.path[state.simulation.step] === 'q6' ? true:false}/>
-                </div>
-                <Xarrow _cpx2Offset={30} _cpx1Offset={30} endAnchor={'right'} startAnchor={'right'}  strokeWidth={3} zIndex={-100} labels="a" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q6.a'} start={'q6'} end={'q5'} />
-                <Xarrow _cpy1Offset={100} _cpx1Offset={200} _cpx2Offset={100} endAnchor={'right'} startAnchor={'bottom'}  strokeWidth={3} zIndex={-100} labels="b" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q6.b'} start={'q6'} end={'q6'} />
-                {/* Q7 */}
-                <div className={`absolute flex justify-center items-center top-[1%] left-[7rem]`}>
-                    <State id={'q7'} state={'q7'} finalState={state.dfa.F.includes('q7')} valid={state.dfa.path[state.simulation.step] === 'q7' ? true:false}/>
-                </div>
-                <Xarrow  endAnchor={'right'} startAnchor={'top'}  strokeWidth={3} zIndex={-100} labels="a" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q1.b'} start={'q1'} end={'q7'} />
-                <Xarrow _cpy1Offset={-400}  _cpx2Offset={-200} _cpx1Offset={-200} endAnchor={'left'} startAnchor={'top'}  strokeWidth={3} zIndex={-100} labels="a,b" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q7.a'} start={'q7'} end={'q8'} />
-                {/* Q8 */}
-                <div className={`absolute flex justify-center items-center top-[80%] left-[7rem]`}>
-                    <State id={'q8'} state={'q8'} finalState={state.dfa.F.includes('q8')} valid={state.dfa.path[state.simulation.step] === 'q8' ? true:false}/>
-                </div>
-                <Xarrow  endAnchor={'right'} startAnchor={'bottom'}  strokeWidth={3} zIndex={-100} labels="b" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q2.a'} start={'q2'} end={'q8'} />
-                <Xarrow _cpy1Offset={400}  _cpx2Offset={-300} _cpx1Offset={-300} endAnchor={'left'} startAnchor={'bottom'}  strokeWidth={3} zIndex={-100} labels="b,a" path='smooth' 
-                        animateDrawing color={theme=== 'Dark'? "#3498DB":"gray"} key={'q8.b'} start={'q8'} end={'q7'} />
-
+                {
+                    state.selection === '1'?<DFA1 theme={theme} state={state}/>:  state.selection === '2' ? <DFA2 theme={theme} state={state}/>:null
+                }
+                
             </Xwrapper>
         </>
     )
--- src/context/problem.tsx
+++ src/context/problem.tsx
@@ -26,6 +26,41 @@ export interface IProblemState{
     currentInput: string;
     
 }
+const prob1 = new DFA(
+            ["a","b"],  
+            // ['q0', 'q1', 'q3', 'q5', 'q7', 'q2', 'q4', 'q6','q8'], 
+            ['q0','q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7','q8'],
+            'q0', 
+            ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7','q8'],
+            {
+                "q0.a":"q1", "q0.b":"q2",
+                "q1.a":"q3", "q1.b":"q7",
+                "q2.a":"q8", "q2.b":"q4",
+                "q3.a":"q5", "q3.b":"q6",
+                "q4.a":"q5", "q4.b":"q6",
+                "q5.a":"q5", "q5.b":"q6",
+                "q6.a":"q5", "q6.b":"q6",
+                "q7.a":"q8", "q7.b":"q8",
+                "q8.a":"q7", "q8.b":"q7",
+            }
+        );
+const prob2 = new DFA(
+        ["0","1"],
+        ['q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7','q8'],
+        'q0',
+        ['q8'],
+        {
+            "q0.0":"q1", "q0.1":"q1",
+            "q1.0":"q3", "q1.1":"q2",
+            "q2.0":"q4", "q2.1":"q5",
+            "q3.0":"q7", "q3.1":"q6",
+            "q4.0":"q7", "q4.1":"q8",
+            "q5.0":"q4", "q5.1":"q8",
+            "q6.0":"q4", "q6.1":"q5",
+            "q7.0":"q8", "q7.1":"q2",
+            "q8.0":"q8", "q8.1":"q8",
+        }
+    );
 
 export type IProblemAction = 
     {type:'SELECT',
@@ -36,7 +71,10 @@ export type IProblemAction =
 const reducer = (state: IProblemState, action: IProblemAction) => {
     switch (action.type){
         case 'SELECT':
-            return {...state, selection: action.payload.selection, problem: action.payload.selection === "1" ? "(aa+bb)(a+b)*(a+b+ab+ba)+(a+b+ab+ba)*(aa+bab)*(a+b+aa)(a+b+bb+aa)*" : "((101)+(111)*+(100)+(1+0+11)*)(1+0+01)*(111+000+101)(1+0)*"};
+            return {...state, 
+                selection: action.payload.selection, 
+                problem: action.payload.selection === "1" ? "(aa+bb)(a+b)*(a+b+ab+ba)+(a+b+ab+ba)*(aa+bab)*(a+b+aa)(a+b+bb+aa)*" : "((101)+(111)*+(100)+(1+0+11)*)(1+0+01)*(111+000+101)(1+0)*",
+                dfa: action.payload.selection=== "1"? prob1: prob2};
         case 'SIMULATE':
             
             state.dfa.execute(state.currentInput);
@@ -84,24 +122,6 @@ const reducer = (state: IProblemState, action: IProblemAction) => {
 }
 
 export const ProblemProvider = ({children}: {children: React.ReactNode}) => {
-    const prob1 = new DFA(
-            ["a","b"],  
-            // ['q0', 'q1', 'q3', 'q5', 'q7', 'q2', 'q4', 'q6','q8'], 
-            ['q0','q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7','q8'],
-            'q0', 
-            ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7','q8'],
-            {
-                "q0.a":"q1", "q0.b":"q2",
-                "q1.a":"q3", "q1.b":"q7",
-                "q2.a":"q8", "q2.b":"q4",
-                "q3.a":"q5", "q3.b":"q6",
-                "q4.a":"q5", "q4.b":"q6",
-                "q5.a":"q5", "q5.b":"q6",
-                "q6.a":"q5", "q6.b":"q6",
-                "q7.a":"q8", "q7.b":"q8",
-                "q8.a":"q7", "q8.b":"q7",
-            }
-        );
     const initialState: IProblemState = {
         selection: "1",
         problem: "(aa+bb)(a+b)*(a+b+ab+ba)+(a+b+ab+ba)*(aa+bab)*(a+b+aa)(a+b+bb+aa)*",
@@ -132,3 +152,5 @@ export const ProblemProvider = ({children}: {children: React.ReactNode}) => {
     );
 };
 export const useProblem = () => React.useContext(ProblemContext);
+
+
